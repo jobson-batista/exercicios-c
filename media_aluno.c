b2fbd415ffd2956d9239acca1bb112d395972a15
Jobson Batista <jobson2084@gmail.com>
2024-05-11 13:17:47 -0300

    media aluno

---
+++ media_aluno.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+    float n1, n2, media;
+
+    printf("Informe a primeira nota: ");
+    scanf("%f", &n1);
+
+    printf("Informe a segunda nota: ");
+    scanf("%f", &n2);
+
+    media = (n1+n2)/2;
+
+    if(media <= 4.0 ) {
+        printf("REPROVADO!\n");
+    } else if (media > 4 && media < 7) {
+        printf("EXAME!\n");
+    } else {
+        printf("APROVADO!\n");
+    }
+    
+}
